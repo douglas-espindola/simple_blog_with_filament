0812a5c2a86d7ab3e39acac9f21a198a16079966
Douglas Espindola <douglas.spindola15@gmail.com>
2023-01-30 14:55:06 -0300

    criado model de post

---
+++ app/Models/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia;
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'content',
+        'is_published'
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
